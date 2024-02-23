28158cd3ea03604b8a5f965fe253d4c1277b693d
sandykr29 <sandeepkumar29774@yahoo.com>
2024-02-24 02:33:19 +0530

    testimonial done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import { About } from './Components/About/About';
+import { Campus } from './Components/Campus/Campus';
 import { Hero } from './Components/Hero/Hero';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Programs } from './Components/Programs/Programs';
+import { Testimonials } from './Components/Testimonials/Testimonials';
 import { Title } from './Components/Title/Title';
 
 
@@ -16,6 +18,11 @@ function App() {
       <Title subTitle="Our Program" title="What we offer"/>
     <Programs/>
     <About/>
+    <Title subTitle="Gallery" title="Campus Photos"/>
+    <Campus/>
+    <Title subTitle="TESTIMONIALS" title="What Students Say"/>
+    <Testimonials/>
+    <Title subTitle="Contact Us" title="What Student Says"/>
      </div>
      
     </div>
