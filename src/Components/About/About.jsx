5236b36e4476a62940ec385ffd8d2f07985848d1
sandykr29 <sandeepkumar29774@yahoo.com>
2024-02-24 01:25:34 +0530

    about-done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,6 +12,7 @@ export const About = () => {
         </div>
         <div className="about-right">
             <h3>ABOUT UNIVERSITY</h3>
+            <h2>Nurturing Tomorrow's Leaders Today</h2>
             <p>Embark on a transformative educational journey with our unversity's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
             <p>With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p>
             <p>Whether you aspire to become a teacher, adminstrator, counselor, or ecucational leader, our diverse range of programs offers the perfect path way to achieve your goals and unlock your full potential in shaping the future of education.</p>
