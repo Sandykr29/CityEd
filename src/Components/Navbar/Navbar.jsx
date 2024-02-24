3f1d4483bd05db018d7a67e9e7cd2ff0c294d000
sandykr29 <sandeepkumar29774@yahoo.com>
2024-02-24 19:14:02 +0530

    media-query upto campus

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import "./Navbar.css";
 import logo from "../../assets/logo.png"
+import { Link } from 'react-scroll';
+import menu_icon from "../../assets/menu-icon.png"
 
 export const Navbar = () => {
-  const [sticky,setSticky]=useState(false)
+  const [sticky,setSticky]=useState(false);
+  const [mobileMenu,setMobileMenu]=useState(false)
   useEffect(()=>{
     window.addEventListener("scroll",()=>{
       window.scrollY>50?setSticky(true):setSticky(false)
     })
   },[])
+
+  const toggleMenu=()=>{
+// setMobileMenu((p)=>!p);
+mobileMenu?setMobileMenu(false):setMobileMenu(true);
+  }
   return (
     <nav className={`container ${sticky?"dark-nav":""}`}>
         <img src={logo} className='logo' alt="logo" />
-        <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact Us</button></li>
+        <ul className={mobileMenu?"":"hide-mobile-menu"}>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
         </ul>
+        <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
