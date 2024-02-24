00374bca15a1b35cd0cb91391d274066c97cf548
sandykr29 <sandeepkumar29774@yahoo.com>
2024-02-24 14:23:00 +0530

    shallow-done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { About } from './Components/About/About';
 import { Campus } from './Components/Campus/Campus';
 import { Contact } from './Components/Contact/Contact';
+import { Footer } from './Components/Footer/Footer';
 import { Hero } from './Components/Hero/Hero';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Programs } from './Components/Programs/Programs';
@@ -25,6 +26,7 @@ function App() {
     <Testimonials/>
     <Title subTitle="Contact Us" title="Get in Touch"/>
     <Contact/>
+    <Footer/>
      </div>
      
     </div>
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>0 2024 Edusity. All rights reserved.</p>
+        <ul>
+            <li>Terms of Services</li>
+            <li>Privacy Policy</li>
+        </ul>
+    </div>
+  )
+}
